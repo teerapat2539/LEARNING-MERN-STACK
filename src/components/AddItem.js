8d686efdb34120a8eed0fafeb5d0bafab892611c
teerapat <teerapat_12345678@hotmail.com>
2017-07-11 21:02:33 +0700

    Created the object of ItemService class and call its sendData method. Pass the input value as a parameter and ready to go.

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ItemService from './ItemService';
 
 class AddItem extends Component {
   constructor(props) {
         super(props);
         this.state = {value: ''};
+        this.addItemService = new ItemService();
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,6 +18,8 @@ class AddItem extends Component {
   handleSubmit(event) {
         alert(this.state.value);
         event.preventDefault();
+        this.addItemService.senndData(this.state.value);
+        this.prop.history.push('/');
   }
   render() {
     return(
