e483b2eaef3f16d6bdc24485938aaa774268108f
teerapat <teerapat_12345678@hotmail.com>
2017-07-11 22:38:41 +0700

    Create Mongoose database connection and also use CORS middleware to request cross-origin domain. Here we are using CORS middleware to bypass the security.

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyPaser = require('body-parser');
 var port = 4200;
+var cors = require('cors');
+
+// Monogoose connection with mongodb
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://<uname>:<pwd>@ds139322.mlab.com:39322/aufinancex')
+    .then(() => {//if all is ok we will be here
+      console.log('start');
+    })
+    .catch(err => { // if error we will be here
+            console.error('App starting error :', err.stack);
+            process.exit(1);
+    });
+//require application specific custom router module
+var itemRouter = require('./src/routes/itemRoutes');
+
+//use middlewares to set view engine and post json data to the server
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded(extentded: true));
+app.use(bodyParser.json());
+
+app.use('/items',itemRouter);
+
+//Start the server
 app.listen(port, function(){
-  console.log('Server is running on port:', port);
-})
-app.get('/', function(req, res){
-    res.send('Hello Express');
+  console.log('Server is running on port: ',port);
 });
