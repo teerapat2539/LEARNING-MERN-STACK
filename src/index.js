a0f31589f69bf2d811b306990f04c1b0c1cfe055
teerapat <teerapat_12345678@hotmail.com>
2017-07-11 16:06:29 +0700

    create addItem.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddItem from './components/AddItem';
+
+ReactDOM.render(
+          <Router>
+              <div>
+                      <Route exact path='/' component={App} />
+                      <Route exact path='/add-item' component={AddItem} />
+              </div>
+          </Router>, document.getElementById('root'));
+
+registerServiceWorker();
