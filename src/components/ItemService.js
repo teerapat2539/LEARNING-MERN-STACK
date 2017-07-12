2ddcbb0665f780da4589a5b76c4d72dc549142c9
teerapat <teerapat_12345678@hotmail.com>
2017-07-12 14:25:43 +0700

    Also, we need to code the update function in ItemService.js file

--- src/components/ItemService.js
+++ src/components/ItemService.js
@@ -5,10 +5,21 @@ class  ItemService {
 
   }
   sendDate(data){
-      axios.post('http://localhost:4200/items/update/'+id, {
+      axios.post('http://localhost:4200/items/add/post', {
         item:data
-      }).then(res => this.setState({ items: res.data}))
-       .catch(err => console.log(err));
+      }).then(function (response){
+        console.log(response);
+      }).catch(function (error){
+        console.log(error);
+      });
+  }
+  
+  updateData(data, id){
+    axios.post('http://localhost:4200/items/update/'+id, {
+      item: data
+    })
+    .then(res => this.setState({ items: res.data }))
+    .catch(err => console.log(err))
   }
 }
 
